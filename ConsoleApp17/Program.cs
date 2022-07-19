66b4b206aa63f482117eded9ad66bbbed6735dbc
Jeffry1990 <109137757+Jeffry1990@users.noreply.github.com>
2022-07-19 20:22:11 +0300

    Add files via upload

---
+++ ConsoleApp17/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ConsoleApp17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+            int number = rand.Next(101);
+            int specifiedNumber1 = 3;
+            int specifiedNumber2 = 5;
+
+            Console.WriteLine("Случайное число: "+number);
+            Console.ReadLine();
+
+            for (int i = specifiedNumber1, j = specifiedNumber2; i <= number; i += 3, j += 5)
+            {
+                    Console.WriteLine(i);
+                    Console.WriteLine(j);
+                    Console.ReadLine();
+                }
+            }
+        }
+    }
